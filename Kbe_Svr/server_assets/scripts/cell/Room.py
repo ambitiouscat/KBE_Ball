53e16c95810c1c9ee0815ef9da24529cac5a9271
AmbitiousCat <1069023394@qq.com>
2019-09-29 16:15:30 +0800

    添加食物

--- Kbe_Svr/server_assets/scripts/cell/Room.py
+++ Kbe_Svr/server_assets/scripts/cell/Room.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 import KBEngine;
 from KBEDebug import *;
+import GameUtils;
 
 class Room(KBEngine.Entity):
     def __init__(self):
         KBEngine.Entity.__init__(self);
         INFO_MSG("Cell Room init ...");
+        pos = GameUtils.randomPosition3d();
+        KBEngine.createEntity("Food",self.spaceID,pos)
