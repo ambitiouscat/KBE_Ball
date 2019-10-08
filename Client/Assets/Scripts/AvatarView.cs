12afd933170420dd3d92ff60bb0c36bd9f81a8ba
AmbitiousCat <1069023394@qq.com>
2019-10-08 10:20:50 +0800

    提交文件

---
+++ Client/Assets/Scripts/AvatarView.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarView : MonoBehaviour {
+
+    [Range(0, 1.0f)]
+    private bool __isMouseDown = false;
+    private Vector3 __lastMouseDir = Vector3.zero;
+    public float m_Speed = 0.1f;
+    public Avatar m_Avatar;
+    public Avatar m_AvatarMain;
+	// Use this for initialization
+	void Start () {
+        Debug.Log("AvatarView::Start ... ");
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (m_AvatarMain == null)
+            return;
+        if (m_AvatarMain.isPlayer())
+        {
+            //Debug.Log("AvatarView::Update ... m_IsPlayer = " + m_IsPlayer);
+            KBEngine.Event.fireIn("updatePlayer", gameObject.transform.position.x, gameObject.transform.position.z,
+                gameObject.transform.position.y, gameObject.transform.rotation.y);
+        }
+        else
+            transform.position = new Vector3(m_AvatarMain.position.x, m_AvatarMain.position.z, m_AvatarMain.position.z);
+
+    }
+
+    void FixedUpdate()
+    {
+        if (m_AvatarMain == null)
+            return;
+        if (!m_AvatarMain.isPlayer())
+            return;
+        //Debug.Log("AvatarView::FixedUpdate ... m_IsPlayer = " + m_IsPlayer);
+        if (Input.GetMouseButtonDown(0))
+        {
+            __isMouseDown = true;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            __isMouseDown = false;
+        }
+        if (__isMouseDown)
+        {
+            if (m_AvatarMain.IsRun <= 0)
+                m_AvatarMain.cellEntityCall.SetRun(1);
+            Vector3 Movement = Camera.main.ScreenToWorldPoint(Input.mousePosition) - this.transform.position;
+            Movement.z = 0.0f;
+            if (Movement.magnitude <= 1.0)
+            {
+                __lastMouseDir = Vector3.zero;
+                return;
+            }
+            Movement.Normalize();
+            __lastMouseDir = Movement;
+        }
+        else if (m_AvatarMain.IsRun > 0)
+            m_AvatarMain.cellEntityCall.SetRun(0);
+        transform.position += __lastMouseDir * m_Speed * 0.1f;
+    }
+}
---
+++ Client/Assets/Scripts/World.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour {
+
+
+    public UnityEngine.GameObject m_AvatarPrefab;
+    public UnityEngine.GameObject m_AvatarMainPrefab;
+    public UnityEngine.GameObject m_FoodPrefab;
+	// Use this for initialization
+	void Start () {
+
+        KBEngine.Event.registerOut("onSelfAvatarEnterWorld", this, "onSelfAvatarEnterWorld");
+        KBEngine.Event.registerOut("onOtherAvatarEnterWorld", this, "onOtherAvatarEnterWorld");
+        KBEngine.Event.registerOut("onLeaveWorld", this, "onLeaveWorld");
+        KBEngine.Event.registerOut("onMoveSpeedChanged", this, "onMoveSpeedChanged");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void onSelfAvatarEnterWorld(KBEngine.Entity entity)
+    {
+        var avatar = Instantiate(m_AvatarMainPrefab);
+        entity.renderObj = avatar;
+        avatar.GetComponent<AvatarView>().m_AvatarMain = entity as Avatar;
+        Debug.Log("World::onSelfAvatarEnterWorld ... id = " + entity.id);
+    }
+
+    public void onOtherAvatarEnterWorld(KBEngine.Entity entity)
+    {
+        Debug.Log("World::onOtherAvatarEnterWorld ... ClassName = " + entity.className + " EntityId =  " + entity.id);
+        
+        if (entity.className == "Avatar")
+        {
+            GameObject PlayerAvt = Instantiate(m_AvatarPrefab);
+            if (PlayerAvt == null)
+                return;
+            entity.renderObj = PlayerAvt;
+            PlayerAvt.GetComponent<AvatarView>().m_Avatar = entity as Avatar;
+        }
+        else if(entity.className == "Food")
+        {
+            GameObject FoodAvt = Instantiate(m_FoodPrefab);
+            if (FoodAvt == null)
+                return;
+            entity.renderObj = FoodAvt;
+            FoodAvt.transform.position = new Vector3(entity.position.x, entity.position.z, entity.position.z);
+        }
+    }
+
+    public void onLeaveWorld(KBEngine.Entity entity)
+    {
+        Debug.Log("World::onLeaveWorld ... classname = " + entity.className + entity.id);
+        Destroy(entity.renderObj as GameObject);
+    }
+
+    public void onMoveSpeedChanged(KBEngine.Entity entity)
+    {
+        Debug.Log("World::onMoveSpeedChanged ... classname = " + entity.className + entity.id + "Speed = " + (entity as Avatar).MoveSpeed);
+        if (entity.renderObj == null)
+            return;
+        //(entity.renderObj as AvatarView).m_Speed = (entity as Avatar).MoveSpeed;
+        //(entity.renderObj as AvatarView).m_Speed这样写会报错，因为AvatarView是entity的一个组件
+        (entity.renderObj as GameObject).GetComponent<AvatarView>().m_Speed = (entity as Avatar).MoveSpeed;
+
+    }
+}
